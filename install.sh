c825b2343bfe0cb74a348b6c4ff0ebb4d7631939
Raphael Fleischlin <git@raeffs.dev>
2024-08-06 17:57:14 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+SCRIPT=$(readlink -f "$0")
+BASEPATH=$(dirname "$SCRIPT")
+HOMEPATH=$(realpath ~)
+
+ln -s -f "$BASEPATH/alacritty/alacritty.toml" "$HOMEPATH/.config/alacritty/alacritty.toml"
+ln -s -f "$BASEPATH/alacritty/colors.toml" "$HOMEPATH/.config/alacritty/colors.toml"
