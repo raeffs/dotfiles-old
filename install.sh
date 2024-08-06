cd3372142afc4c777acb8e08899a2a54e8334da7
Raphael Fleischlin <git@raeffs.dev>
2024-08-06 18:45:01 +0200

    make tab completion case insensitive

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ SCRIPT=$(readlink -f "$0")
 BASEPATH=$(dirname "$SCRIPT")
 HOMEPATH=$(realpath ~)
 
+ln -s -f "$BASEPATH/.inputrc" "$HOMEPATH/.inputrc"
+
 ln -s -f "$BASEPATH/alacritty/alacritty.toml" "$HOMEPATH/.config/alacritty/alacritty.toml"
 ln -s -f "$BASEPATH/alacritty/colors.toml" "$HOMEPATH/.config/alacritty/colors.toml"
